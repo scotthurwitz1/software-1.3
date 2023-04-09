16791cda11e5934e9a960eb21aab071d2cdc8b81
Scotthurwitz1 <hurwitzscott@gmail.com>
2023-04-09 03:14:10 -0400

    Add part form

--- src/Controller/AddPartFormController.java
+++ src/Controller/AddPartFormController.java
@@ -1,7 +1,9 @@
 package Controller;
 
+import Model.InHouse;
 import Model.Inventory;
 import static Model.Inventory.getAllParts;
+import Model.Outsourced;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import main.Switcher;
@@ -16,6 +19,7 @@ import main.Switcher;
 public class AddPartFormController implements Initializable {
     
     Switcher switcher = new Switcher();
+    boolean outsourced = false;
 
     @FXML
     private Button cancelBtn;
@@ -46,6 +50,26 @@ public class AddPartFormController implements Initializable {
 
     @FXML
     private Button saveBtn;
+    
+    @FXML
+    private Label machineIdLbl;
+    
+    // Handle In house / outsourced button menue changes
+    @FXML
+    void onActionOutsourcedButton(ActionEvent event){
+        
+        machineIdLbl.setText("Company Name");
+        outsourced = true;
+        
+    }
+    
+    @FXML
+    void onActionInHouseBtn (ActionEvent event){
+        
+        machineIdLbl.setText("Machine ID");
+        outsourced = false;
+        
+    }
 
     @FXML
     void onActionCancelPart(ActionEvent event) throws IOException {
@@ -57,20 +81,28 @@ public class AddPartFormController implements Initializable {
     @FXML
     void onActionSavePart(ActionEvent event) {
         
+        // Parse fields on form
         int id = Inventory.getAllParts().size()+1;
         String name = nameTxt.getText();
         int inv = Integer.parseInt(invTxt.getText());
         float price = Float.parseFloat(priceTxt.getText());
         int max = Integer.parseInt(maxTxt.getText());
         int min = Integer.parseInt(minTxt.getText());
-        int machineId = Integer.parseInt(machineIdTxt.getText());
+        
+        if (outsourced = false){
+            int machineId = Integer.parseInt(machineIdTxt.getText());
+            Inventory.addPart(new InHouse(id, name, price, inv, min, max, machineId));
+        }
+        else {
+            String companyName = machineIdTxt.getText();
+            Inventory.addPart(new Outsourced(id, name, price, inv, min, max, companyName));
+        }
         
     }
     
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
- 
+        
     }
 
 }
--- src/Model/Outsourced.java
+++ src/Model/Outsourced.java
@@ -6,7 +6,7 @@ public class Outsourced extends Part{
     private String companyName;
 
     // Declare Methods
-    Outsourced(int id, String name, double price, int stock, int min, int max, String companyName){
+    public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName){
         super(id, name, price, stock, min, max);
         this.companyName = companyName;
     }
