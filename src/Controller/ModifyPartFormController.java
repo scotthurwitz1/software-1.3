81d03343acad8dfffb30152b5100e4cc7d8de48f
scott <rfudbn@gmail.com>
2023-04-17 01:57:13 -0400

    Add modify part functionality

--- src/Controller/ModifyPartFormController.java
+++ src/Controller/ModifyPartFormController.java
@@ -1,8 +1,10 @@
 package Controller;
 
 import Model.InHouse;
+import Model.Inventory;
 import Model.Outsourced;
 import Model.Part;
+import Model.Product;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,6 +19,7 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+import static main.Methods.update;
 import main.Switcher;
 
 public class ModifyPartFormController implements Initializable {
@@ -24,6 +27,7 @@ public class ModifyPartFormController implements Initializable {
     Switcher switcher = new Switcher();
     Stage stage;
     Parent scene;
+    boolean outsourced = false;
 
     @FXML
     private Button cancelBtn;
@@ -63,6 +67,23 @@ public class ModifyPartFormController implements Initializable {
 
     @FXML
     private ToggleGroup sourceTG;
+    
+      // Handle In house / outsourced button menue changes
+    @FXML
+    void onActionOutsourcedButton(ActionEvent event){
+        
+        machineIdLbl.setText("Company Name");
+        outsourced = true;
+        
+    }
+    
+    @FXML
+    void onActionInHouseButton (ActionEvent event){
+        
+        machineIdLbl.setText("Machine ID");
+        outsourced = false;
+        
+    }
 
     @FXML
     void onActionCancelPart(ActionEvent event) throws IOException {
@@ -72,7 +93,27 @@ public class ModifyPartFormController implements Initializable {
     }
 
     @FXML
-    void onActionSavePart(ActionEvent event) {
+    void onActionSavePart(ActionEvent event) throws IOException {
+        
+        int id = Integer.parseInt(idTxt.getText());
+        String name = nameTxt.getText();
+        int inv = Integer.parseInt(invTxt.getText());
+        float price = Float.parseFloat(priceTxt.getText());
+        int max = Integer.parseInt(maxTxt.getText());
+        int min = Integer.parseInt(minTxt.getText());
+        
+        if (outsourced = false){
+            int machineId = Integer.parseInt(machineIdTxt.getText());
+            InHouse part1 = new InHouse(id, name, price, inv, min, max, machineId);
+            update(id, part1);
+        }
+        else {
+            String companyName = machineIdTxt.getText();
+            Outsourced part1 = new Outsourced(id, name, price, inv, min, max, companyName);
+            update(id, part1);
+        }
+        
+        switcher.screen("/View/mainForm.fxml", event);
         
     }
     
