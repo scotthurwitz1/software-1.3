6a894309ce1da6d309a32437b52d5627aa8607e0
scott <rfudbn@gmail.com>
2023-04-20 15:49:04 -0400

    modify product search

--- src/Controller/ModifyProductFormController.java
+++ src/Controller/ModifyProductFormController.java
@@ -13,11 +13,14 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import main.Methods;
+import static main.Methods.getAllFilteredParts;
 import static main.Methods.selectProd;
 import static main.Methods.update;
 import main.Switcher;
@@ -99,7 +102,14 @@ public class ModifyProductFormController implements Initializable {
     @FXML
     void onActionSearchTxt (ActionEvent event)
     {
-        
+        partListTbl.setItems(Methods.filterParts(searchTxt.getText()));
+        if (getAllFilteredParts().isEmpty())
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setContentText("Part not found.");
+            alert.showAndWait();
+        } 
     }
 
     @FXML
@@ -107,7 +117,6 @@ public class ModifyProductFormController implements Initializable {
         
         Part part = partListTbl.getSelectionModel().getSelectedItem();
         associatedParts1.add(part);
-        System.out.println("part added " + associatedParts1);
                 
     }
 
@@ -128,7 +137,6 @@ public class ModifyProductFormController implements Initializable {
         
         Part part = relPartTbl.getSelectionModel().getSelectedItem();
         associatedParts1.remove(part);
-        System.out.println(associatedParts1);
 
     }
 
@@ -175,8 +183,6 @@ public class ModifyProductFormController implements Initializable {
         for(Part part : prod.getAllAssociatedParts()){
             associatedParts1.add(part);
         }
-        
-        System.out.println("prod sent, ass parts = " + associatedParts1);
 
     }
     
@@ -204,8 +210,6 @@ public class ModifyProductFormController implements Initializable {
         relPartPriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
         relPartInvCol.setCellValueFactory(new PropertyValueFactory<>("stock"));
         relPartNameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
-        
-        System.out.println("initialized");
 
      
     }
